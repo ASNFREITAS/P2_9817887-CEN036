37f842e3dcdba962c785dfe805a66d5bb1f5af00
ASNFREITAS <91679612+ASNFREITAS@users.noreply.github.com>
2022-12-15 23:22:24 -0300

    Update Ex4.py

--- Ex4.py
+++ Ex4.py
@@ -8,9 +8,9 @@ Contador = 0  #atribuição de valor 0 pra variavel Contador
 
 while Contador <= 10:            # loop while que fara a somatoria das notas
     num = int(input("Digite uma nota: "))  #pega o imput do usuario para a nota
-    Total = num + Total
-    Contador = Contador + 1
+    Total = num + Total  # acrescenta a nota á variavel Total
+    Contador = Contador + 1 # adiciona 1 na Variavel Contador
     
-Nota = Total / 10    
-print(Nota)
+Nota = Total / 10    #Cria a variavel nota que recebera o valor da media
+print(Nota)  #printa a variavel Nota
 
